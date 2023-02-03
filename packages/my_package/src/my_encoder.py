49acbed43c30fc2d72c2d878b1713e144e22079d
Nadeen <nadeen@ualberta.ca>
2023-02-02 23:45:57 -0700

    add updates

--- packages/my_package/src/my_encoder.py
+++ packages/my_package/src/my_encoder.py
@@ -21,120 +21,118 @@ class OdometryNode(DTROS):
     # Get static parameters
     self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
     self._total_ticks = 135
+    self._L = 0.05
+    # Nadeen's Robot
+    self.right_velocity = 0.4297693967819214
+    self.left_velocity = 0.5252736806869507
+
+    # Celina's Robot
+    # TODO: add
+    self.right_velocity = 0.4297693967819214
+    self.left_velocity = 0.5252736806869507
 
     # Initialize the executed commands message
     self.msg_wheels_cmd = WheelsCmdStamped()
     self.msg_wheels_cmd.header = Header()
 
-    # Subscribing to the wheel encoders
+    # Subscribers
     self.sub_encoder_ticks_left = rospy.Subscriber(f"/{self.veh_name}/left_wheel_encoder_node/tick", WheelEncoderStamped, self.left_callback)
     self.sub_encoder_ticks_right = rospy.Subscriber(f"/{self.veh_name}/right_wheel_encoder_node/tick", WheelEncoderStamped, self.right_callback)
-    # self.sub_executed_commands = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.command_callback)
+    self.sub_executed_commands = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.command_callback)
 
-    # /csc22905/left_wheel_encoder_node/tick /csc22905/right_wheel_encoder_node/tick /csc22905/wheels_driver_node/wheels_cmd_executed
     # Publishers
-    # self.pub_integrated_distance_left = rospy.Publisher('distance_travelled_left', Float32, queue_size=10)
-    # self.pub_integrated_distance_right = rospy.Publisher('distance_travelled_left', Float32, queue_size=10)
+    self.pub_integrated_distance = rospy.Publisher('distance_travelled', Float32, queue_size=10)
     self.pub_wheel_command = rospy.Publisher(f'/{self.veh_name}/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
 
-    self.initialized_left = False
-    self.initial_ticks_left = 0
-    self.initialized_right = False
-    self.initial_ticks_right = 0
-    self.distance_travelled_left = 0
-    self.distance_travelled_right = 0
-
-    self.right_velocity = 0.4297693967819214
-    self.left_velocity = 0.5252736806869507
+    self.initial_ticks_left = -1
+    self.current_ticks_left = 0
+    self.initial_ticks_right = -1
+    self.current_ticks_right = 0
 
     self.log("Initialized")
   
-  def left_callback(self, data):
-    # rospy.loginfo("Left data: %s", data.data)
-    if not self.initialized_left:
-      self.initialized_left = True
-      self.initial_ticks_left = data.data
-    else:
-      self.distance_travelled_left = 2 * math.pi * self._radius * abs(data.data - self.initial_ticks_left) / self._total_ticks
-  
-  def right_callback(self, data):
-    # rospy.loginfo("Right data: %s", data.data)
-    if not self.initialized_right:
-      self.initialized_right = True
-      self.initial_ticks_right = data.data
-    else:
-      self.distance_travelled_right = 2 * math.pi * self._radius * abs(data.data - self.initial_ticks_right) / self._total_ticks
-  
+  def left_callback(self, msg):
+    # rospy.loginfo("Left data: %s", msg.data)
+    self.current_ticks_left = msg.data
+    if self.initial_ticks_left < 0: self.initial_ticks_left = msg.data # set initial ticks if uninitialized
+
+  def right_callback(self, msg):
+    # rospy.loginfo("Right data: %s", msg.data)
+    self.current_ticks_right = msg.data
+    if self.initial_ticks_right < 0: self.initial_ticks_right = msg.data # set initial ticks if uninitialized
+
   def command_callback(self, data):
-    rospy.loginfo("Header sequence: %s", data.header.seq)
-    rospy.loginfo("Header stamp: %s", data.header.stamp)
-    rospy.loginfo("Header frame id: %s", data.header.frame_id)
-    rospy.loginfo("Header sequence: %s", data.header.seq)
+    # rospy.loginfo("Header sequence: %s", data.header.seq)
+    # rospy.loginfo("Header stamp: %s", data.header.stamp)
+    # rospy.loginfo("Header frame id: %s", data.header.frame_id)
+    # rospy.loginfo("Header sequence: %s", data.header.seq)
     rospy.loginfo("Left velocity data: %s", data.vel_left)
     rospy.loginfo("Right velocity data: %s", data.vel_right)
-
-  def cb_encoder_data(self, wheel, msg):
-    """
-    Update encoder distance information from ticks.
-    """
-
-  def cb_executed_commands(self, msg):
-    """
-    Use the executed commands to determine the direction of travel of each wheel.
-    """
 
   def runDistancePublisher(self):
     # publish message every 2 seconds
     rate = rospy.Rate(0.5) # 0.5Hz
     while not rospy.is_shutdown():
-      rospy.loginfo("Left distance: %s", self.distance_travelled_left)
-      self.pub_integrated_distance_left.publish(self.distance_travelled_left)
-
-      rospy.loginfo("Right distance: %s", self.distance_travelled_right)
-      self.pub_integrated_distance_right.publish(self.distance_travelled_right)
+      self.pub_integrated_distance.publish(self.distanceTravelled())
       rate.sleep()
 
   def run(self):
     # publish message every 1 second
-    rate = rospy.Rate(1) # 1
-    # self.driver.set_wheels_speed(left=self.left_velocity, right=self.right_velocity)
-    forward = True
+    rate = rospy.Rate(10) # 1
+    state = "forward"
     while not rospy.is_shutdown():
-      if forward:
-        if max(self.distance_travelled_right, self.distance_travelled_left) >= 1.25:
-          self.msg_wheels_cmd.header.stamp = rospy.Time.now()
-          self.msg_wheels_cmd.vel_left = 0.0
-          self.msg_wheels_cmd.vel_right = 0.0
-          self.pub_wheel_command.publish(self.msg_wheels_cmd)
-          forward = False
-          # rospy.loginfo("Publishing command: %s", self.msg_wheels_cmd)
+      if state == "forward":
+        if self.distanceTravelled() < 1:
+          self.publishCommand(self.left_velocity, self.right_velocity)
         else:
-          self.msg_wheels_cmd.header.stamp = rospy.Time.now()
-          self.msg_wheels_cmd.vel_left = self.left_velocity
-          self.msg_wheels_cmd.vel_right = self.right_velocity
-          self.pub_wheel_command.publish(self.msg_wheels_cmd)
-        rospy.loginfo("Publishing command: %s", self.msg_wheels_cmd)
-      else:
-        if max(self.distance_travelled_right, self.distance_travelled_left) >= 2.5:
-          self.msg_wheels_cmd.header.stamp = rospy.Time.now()
-          self.msg_wheels_cmd.vel_left = 0.0
-          self.msg_wheels_cmd.vel_right = 0.0
-          self.pub_wheel_command.publish(self.msg_wheels_cmd)
-          break
-          # rospy.loginfo("Publishing command: %s", self.msg_wheels_cmd)
+          self.publishCommand(0.0, 0.0)
+          self.resetInitialTicks()
+          state = "turn"
+      if state == "turn":
+        # TODO: adjust this, overturns/underturns at times
+        if self.angleTurned() < math.pi / 2:
+          self.publishCommand(-self.left_velocity, self.right_velocity)
         else:
-          self.msg_wheels_cmd.header.stamp = rospy.Time.now()
-          self.msg_wheels_cmd.vel_left = -self.left_velocity
-          self.msg_wheels_cmd.vel_right = -self.right_velocity
-          self.pub_wheel_command.publish(self.msg_wheels_cmd)
-        rospy.loginfo("Publishing command: %s", self.msg_wheels_cmd)
+          self.publishCommand(0.0, 0.0)
+          self.resetInitialTicks()
+          state = "forward"
+      rospy.loginfo("Publishing command: %s", self.msg_wheels_cmd)
       rate.sleep()
+    self.publishCommand(0.0, 0.0)
+
+  def publishCommand(self, left_vel, right_vel):
+    self.msg_wheels_cmd.header.stamp = rospy.Time.now()
+    self.msg_wheels_cmd.vel_left = left_vel
+    self.msg_wheels_cmd.vel_right = right_vel
+    self.pub_wheel_command.publish(self.msg_wheels_cmd)
+
+  def distanceTravelledRight(self):
+    return 2 * math.pi * self._radius * (self.current_ticks_right - self.initial_ticks_right) / self._total_ticks
+  
+  def resetInitialRightTicks(self):
+    self.initial_ticks_right = self.current_ticks_right
+  
+  def distanceTravelledLeft(self):
+    return 2 * math.pi * self._radius * (self.current_ticks_left - self.initial_ticks_left) / self._total_ticks
+  
+  def resetInitialLeftTicks(self):
+    self.initial_ticks_left = self.current_ticks_left
+
+  def resetInitialTicks(self):
+    self.resetInitialLeftTicks()
+    self.resetInitialRightTicks()
+  
+  def distanceTravelled(self):
+    return (self.distanceTravelledRight() + self.distanceTravelledLeft()) / 2
+  
+  def angleTurned(self):
+    return abs(self.distanceTravelledRight() - self.distanceTravelledLeft()) / (2 * self._L)
 
 if __name__ == '__main__':
   node = OdometryNode(node_name='my_encoder_node')
   # Run the publisher
-  # node.runDistancePublisher()
+  node.runDistancePublisher()
   # Keep it spinning to keep the node alive
-  node.run()
+  # node.run()
   rospy.spin()
   rospy.loginfo("wheel_encoder_node is up and running...")
