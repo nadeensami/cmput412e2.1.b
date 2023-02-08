ead84ef320cd6115278360db58fbcf47d3a50ade
Nadeen <nadeen@ualberta.ca>
2023-02-08 16:21:17 -0700

    server/client working

--- packages/my_package/src/led_server.py
+++ packages/my_package/src/led_server.py
@@ -4,8 +4,10 @@ import sys
 # from my_package.srv import ColorServiceResponse, ColorService
 # from std_msgs.msg import Bool, String
 from std_srvs.srv import SetBoolResponse
-from duckietown_msgs.srv import SetFSMState
+from duckietown_msgs.srv import SetFSMState, SetFSMStateResponse
+from duckietown_msgs.msg import LEDPattern
 from duckietown.dtros import DTROS, NodeType
+from std_msgs.msg import Header
 
 NAME = 'led_server'
 
@@ -13,13 +15,21 @@ class LEDServerNode(DTROS):
   def __init__(self, node_name):
     super(LEDServerNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
     self.service = rospy.Service(NAME, SetFSMState, self.change_color)
+    self.veh_name = rospy.get_namespace().strip("/")
+
+    self.publisher = rospy.Publisher(f'/{self.veh_name}/led_emitter_node', LEDPattern)
+
+    self.pattern = LEDPattern()
+    self.pattern.header = Header()
 
   def change_color(self, request):
-    print("got request: ", request)
-    response = SetBoolResponse()
-    response.success = True
-    response.message = 'Successfully set color'
-    return response
+    print("got request: ", request.state)
+
+    self.pattern.header.stamp = rospy.Time.now()
+
+    self.publisher.publish(LEDPattern())
+
+    return SetFSMStateResponse()
 
 if __name__ == '__main__':
   # Create the LEDServerNode object
--- packages/my_package/src/my_test_client.py
+++ packages/my_package/src/my_test_client.py
@@ -3,7 +3,7 @@ import rospy
 # from my_package.srv import ColorServiceResponse, ColorService
 # from std_msgs.msg import Bool, String
 # from std_srvs.srv import SetBoolResponse
-# from duckietown_msgs.srv import SetFSMState
+from duckietown_msgs.srv import SetFSMState
 from duckietown.dtros import DTROS, NodeType
 
 NAME = 'led_server'
@@ -12,15 +12,14 @@ class LEDClientNode(DTROS):
   def __init__(self, node_name):
     super(LEDClientNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
 
-    print('client node up')
-    # rospy.wait_for_service(NAME)
+    rospy.wait_for_service(NAME)
 
-    # try:
-    #   change_color = rospy.ServiceProxy(NAME, SetFSMState)
-    #   resp = change_color("purple")
-    #   print("got result: ", resp)
-    # except rospy.ServiceException as e:
-    #   print("Service call failed: %s" % e)
+    try:
+      change_color = rospy.ServiceProxy(NAME, SetFSMState)
+      resp = change_color("purple")
+      print("got result: ", resp)
+    except rospy.ServiceException as e:
+      print("Service call failed: %s" % e)
 
 if __name__ == '__main__':
   # Create the LEDClientNode object
