97805e8c3073d21d080c7a581226e7218b8ae16d
csheng2 <42281621+csheng2@users.noreply.github.com>
2023-02-11 15:03:41 -0700

    add odometry files

---
+++ packages/my_package/odometry results/read_odometry.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+file = open("odometry-2.txt", "r")
+
+x = []
+y = []
+theta = []
+
+for line in file:
+  remove_parenthesis = line[1:len(line)-2]
+  data = remove_parenthesis.split(", ")
+  x.append(float(data[0]))
+  y.append(float(data[1]))
+  theta.append(float(data[2]))
+
+plt.plot(x,y)
+plt.show()
